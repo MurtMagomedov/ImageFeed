3bc821e863856f5cce561a5720e73720842af11c
Murtazali <m.magomedov.a@mail.ru>
2024-07-12 21:11:25 +0300

    sprint_09

--- Image Feed/Profile/ProfileViewController.swift
+++ Image Feed/Profile/ProfileViewController.swift
@@ -9,20 +9,96 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
-    @IBOutlet private var avatarImage: UIImageView!
-    @IBOutlet private var exitButton: UIButton!
-    @IBOutlet private var descriptionLabel: UILabel!
-    @IBOutlet private var userLabel: UILabel!
-    @IBOutlet private var nameLabel: UILabel!
+    private var avatarImage: UIImageView?
+    private var exitButton: UIButton?
+    private var descriptionLabel: UILabel?
+    private var userLabel: UILabel?
+    private var nameLabel: UILabel?
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
+        avatar()
+        name()
+        user()
+        description()
+        exit()
     }
     
-    @IBAction func exitButtonAction(_ sender: Any) {
+    @objc private func exitButtonAction() {
         
     }
     
+    private func avatar() {
+        let avatar = UIImageView()
+        avatar.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(avatar)
+        avatar.image = UIImage(named: "Avatar")
+        
+        avatar.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        avatar.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        avatar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16).isActive = true
+        avatar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        self.avatarImage = avatar
+    }
+    
+    private func name() {
+        let name = UILabel()
+        name.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(name)
+        name.textColor = .white
+        
+        guard let avatar = avatarImage else {return}
+        name.text = "Суворов Александр"
+        name.topAnchor.constraint(equalTo: avatar.safeAreaLayoutGuide.bottomAnchor, constant: 8).isActive = true
+        name.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        self.nameLabel = name
+    }
+    
+    private func user() {
+        let user = UILabel()
+        user.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(user)
+        user.textColor = .gray
+        
+        user.text = "@aleksandr-su"
+        guard let name = nameLabel else {return}
+        user.topAnchor.constraint(equalTo: name.safeAreaLayoutGuide.bottomAnchor, constant: 8).isActive = true
+        user.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        self.userLabel = user
+    }
+    
+    private func description () {
+        let description = UILabel()
+        description.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(description)
+        description.textColor = .white
+        
+        description.text = "Hello World"
+        guard let user = userLabel else {return}
+        description.topAnchor.constraint(equalTo: user.safeAreaLayoutGuide.bottomAnchor, constant: 8).isActive = true
+        description.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        self.descriptionLabel = description
+    }
+    
+    private func exit() {
+        let image = UIImage(named: "Exit_Button")
+        guard let image = image else {return}
+        
+        let exit = UIButton.systemButton(
+            with: image,
+            target: self,
+            action: #selector(exitButtonAction)
+        )
+        exit.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(exit)
+        exit.tintColor = UIColor(named: "YPRedColor")
+        
+        exit.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        exit.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        exit.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16).isActive = true
+        exit.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
+        self.exitButton = exit
+    }
 }
